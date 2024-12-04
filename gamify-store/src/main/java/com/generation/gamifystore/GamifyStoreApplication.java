f9ff64b55eb2a8e30efcddb4d07ab3c309cd2e0c
annavighi <vighianna@gmail.com>
2024-12-04 10:10:11 -0300

    Preparação do ambiente

---
+++ gamify-store/src/main/java/com/generation/gamifystore/GamifyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.generation.gamifystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifyStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamifyStoreApplication.class, args);
+	}
+
+}
